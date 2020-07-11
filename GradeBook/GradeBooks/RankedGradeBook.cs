af866d61ef0cec57790a17d49ed53c6d2144453c
kalkis <dylan-creighton@outlook.com>
2020-07-11 20:59:45 +0100

    GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,4 @@
+using System;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -7,6 +8,28 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Need 5 or more students for Ranked gradebook");
+            }
+            switch(averageGrade)
+            {
+                case var d when d >= 80.0:
+                  return 'A';
+                case var d when d >= 60.0:
+                  return 'B';
+                case var d when d >= 40.0:
+                  return 'C';
+                case var d when d >= 20.0:
+                  return 'D';
+                default:
+                  return 'F';
+            }
+           
         }
     }
 }
