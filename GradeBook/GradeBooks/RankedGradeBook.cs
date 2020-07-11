e33538985fb613bdac6946e1c30f82aed1cce3a4
kalkis <dylan-creighton@outlook.com>
2020-07-11 21:30:11 +0100

    override statistics methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,24 +12,44 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count < 5)
+            if (Students.Count < 5)
             {
                 throw new InvalidOperationException("Need 5 or more students for Ranked gradebook");
             }
-            switch(averageGrade)
+            switch (averageGrade)
             {
                 case var d when d >= 80.0:
-                  return 'A';
+                    return 'A';
                 case var d when d >= 60.0:
-                  return 'B';
+                    return 'B';
                 case var d when d >= 40.0:
-                  return 'C';
+                    return 'C';
                 case var d when d >= 20.0:
-                  return 'D';
+                    return 'D';
                 default:
-                  return 'F';
+                    return 'F';
             }
-           
+
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
